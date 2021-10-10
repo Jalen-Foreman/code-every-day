f3f7f68e90021eb66cdb64193dc421d8bd2e0d0f
Jalen-Foreman <78430591+Jalen-Foreman@users.noreply.github.com>
2021-10-10 02:59:39 -0400

    another frequency chally

--- Anagrams.js
+++ Anagrams.js
@@ -17,86 +17,31 @@ const validAnagram = (str1, str2) => {
         else,
         Return boolean stating if the two strings are anagrams or not.
     */
-   if(str1.length !== str2.length) {
-       return false;
-   }
-    const charCount1 = {};
-    const charCount2 = {};
-    for(let str of str1) {
-        str = str.toLowerCase()
-        charCount1[str] = (charCount1[str] || 0) + 1;
-    }
-    for(let str of str2) {
-        str = str.toLowerCase()
-        charCount2[str] = (charCount2[str] || 0) + 1;
-    }
-    const keys = Object.keys(charCount1)
-    for(let key of keys) {
-        if(charCount1[key] !== charCount2[key]) {
-            return false
-        } 
-    }
-    return true
+  if(str1.length !== str2.length) {
+      return false
+  }
+  const str1Obj = {}
+  const str2Obj = {}
+  let word1 = str1.toLowerCase()
+  let word2 = str2.toLowerCase()
+  for(let char of word1) {
+    str1Obj[char] = (str1Obj[char] || 0) + 1
+  }
+  for(let char of word2) {
+      str2Obj[char] = (str2Obj[char] || 0) + 1
+  }
+
+  for(let key in str1Obj) {
+      if(str1Obj[key] !== str2Obj[key]) {
+          return false
+      }
+      if(!(key in str2Obj)){
+          return false
+      }
+  }
+  return true
 }
 console.log(validAnagram('Iceman', 'Cinema')) // true
 console.log(validAnagram('Bakers', 'Cinema')); // false
 console.log(validAnagram('tar', 'Rat')); // true
 console.log(validAnagram('texttwisttime', 'timetwisttext')); // true
-
-// Solve & Simplify
-// const obj = {
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-   
-
-// };
-
-// const obj2 = {
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-// 	jalen: 2,
-	
-// };
-
-// keyVal1 = Object.keys(obj);
-// function check() {
-//     for(let key of keyVal1) {
-//       if(obj[key] !== obj2[key]) {
-//           return false
-//       } 
-//     }
-//     return true
-// }
-// console.log(check())
-
-// Modified Solution
-
-const validAnagram = (str1, str2) => {
-	/*
-        Get strings.
-        If strings are not the same length, return false
-        Make an object that checks the char count of each letter in the strings.
-        Compare the two objects.
-        If the first object doesn't contain the same letters and the same amount of letters as the second object return false, 
-        else,
-        Return boolean stating if the two strings are anagrams or not.
-    */
-	if (str1.length !== str2.length) {
-		return false;
-	}
-	const charCount = {};
-	
-	for(let str of str1) {
-		str = str.toLowerCase();
-		charCount[str] = (charCount[str] || 0) + 1;
-	}
-	for(let str of str2) {
-        let letter = charCount[str]
-    }
-};
